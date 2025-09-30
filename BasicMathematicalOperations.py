0122e20dbc63ff5f9d375c063b9c62792b36e9d7
subho oraon <147547085+ROLEX556@users.noreply.github.com>
2025-09-30 17:45:04 +0530

    Add files via upload

---
+++ BasicMathematicalOperations.py
@@ -0,0 +1,17 @@
+#Takes wo numbers as input from the user
+number1=int(input("Enter the first number: "))
+number2=int(input("Enter the second number: "))
+
+#addition
+addition = number1 + number2
+print("Addition: ",addition)
+#subtraction
+subtraction = number1 - number2
+print("Subtraction: ",subtraction)
+#multiplication
+multiplication = number1 * number2
+print("Multiplication: ",multiplication)
+#division 
+division = number1 / number2
+print("Division: ",division)
+5
